1906f18e7c8584fc318d4366ee0967799650a205
nakanishishuntaro <148457745+NakanishiShuntaro@users.noreply.github.com>
2025-09-09 23:41:56 +0900

    rename

--- _deps/picotool-subbuild/picotool-populate-prefix/tmp/picotool-populate-mkdirs.cmake
+++ _deps/picotool-subbuild/picotool-populate-prefix/tmp/picotool-populate-mkdirs.cmake
@@ -4,19 +4,19 @@
 cmake_minimum_required(VERSION 3.5)
 
 file(MAKE_DIRECTORY
-  "/Users/a81802/Documents/Programs/test1/_deps/picotool-src"
-  "/Users/a81802/Documents/Programs/test1/_deps/picotool-build"
-  "/Users/a81802/Documents/Programs/test1/_deps/picotool-subbuild/picotool-populate-prefix"
-  "/Users/a81802/Documents/Programs/test1/_deps/picotool-subbuild/picotool-populate-prefix/tmp"
-  "/Users/a81802/Documents/Programs/test1/_deps/picotool-subbuild/picotool-populate-prefix/src/picotool-populate-stamp"
-  "/Users/a81802/Documents/Programs/test1/_deps/picotool-subbuild/picotool-populate-prefix/src"
-  "/Users/a81802/Documents/Programs/test1/_deps/picotool-subbuild/picotool-populate-prefix/src/picotool-populate-stamp"
+  "/Users/a81802/Documents/Programs/groupc_fm/_deps/picotool-src"
+  "/Users/a81802/Documents/Programs/groupc_fm/_deps/picotool-build"
+  "/Users/a81802/Documents/Programs/groupc_fm/_deps/picotool-subbuild/picotool-populate-prefix"
+  "/Users/a81802/Documents/Programs/groupc_fm/_deps/picotool-subbuild/picotool-populate-prefix/tmp"
+  "/Users/a81802/Documents/Programs/groupc_fm/_deps/picotool-subbuild/picotool-populate-prefix/src/picotool-populate-stamp"
+  "/Users/a81802/Documents/Programs/groupc_fm/_deps/picotool-subbuild/picotool-populate-prefix/src"
+  "/Users/a81802/Documents/Programs/groupc_fm/_deps/picotool-subbuild/picotool-populate-prefix/src/picotool-populate-stamp"
 )
 
 set(configSubDirs )
 foreach(subDir IN LISTS configSubDirs)
-    file(MAKE_DIRECTORY "/Users/a81802/Documents/Programs/test1/_deps/picotool-subbuild/picotool-populate-prefix/src/picotool-populate-stamp/${subDir}")
+    file(MAKE_DIRECTORY "/Users/a81802/Documents/Programs/groupc_fm/_deps/picotool-subbuild/picotool-populate-prefix/src/picotool-populate-stamp/${subDir}")
 endforeach()
 if(cfgdir)
-  file(MAKE_DIRECTORY "/Users/a81802/Documents/Programs/test1/_deps/picotool-subbuild/picotool-populate-prefix/src/picotool-populate-stamp${cfgdir}") # cfgdir has leading slash
+  file(MAKE_DIRECTORY "/Users/a81802/Documents/Programs/groupc_fm/_deps/picotool-subbuild/picotool-populate-prefix/src/picotool-populate-stamp${cfgdir}") # cfgdir has leading slash
 endif()
--- picotool/tmp/picotoolBuild-mkdirs.cmake
+++ picotool/tmp/picotoolBuild-mkdirs.cmake
@@ -4,19 +4,19 @@
 cmake_minimum_required(VERSION 3.5)
 
 file(MAKE_DIRECTORY
-  "/Users/a81802/Documents/Programs/test1/_deps/picotool-src"
-  "/Users/a81802/Documents/Programs/test1/_deps/picotool-build"
-  "/Users/a81802/Documents/Programs/test1/_deps"
-  "/Users/a81802/Documents/Programs/test1/picotool/tmp"
-  "/Users/a81802/Documents/Programs/test1/picotool/src/picotoolBuild-stamp"
-  "/Users/a81802/Documents/Programs/test1/picotool/src"
-  "/Users/a81802/Documents/Programs/test1/picotool/src/picotoolBuild-stamp"
+  "/Users/a81802/Documents/Programs/groupc_fm/_deps/picotool-src"
+  "/Users/a81802/Documents/Programs/groupc_fm/_deps/picotool-build"
+  "/Users/a81802/Documents/Programs/groupc_fm/_deps"
+  "/Users/a81802/Documents/Programs/groupc_fm/picotool/tmp"
+  "/Users/a81802/Documents/Programs/groupc_fm/picotool/src/picotoolBuild-stamp"
+  "/Users/a81802/Documents/Programs/groupc_fm/picotool/src"
+  "/Users/a81802/Documents/Programs/groupc_fm/picotool/src/picotoolBuild-stamp"
 )
 
 set(configSubDirs )
 foreach(subDir IN LISTS configSubDirs)
-    file(MAKE_DIRECTORY "/Users/a81802/Documents/Programs/test1/picotool/src/picotoolBuild-stamp/${subDir}")
+    file(MAKE_DIRECTORY "/Users/a81802/Documents/Programs/groupc_fm/picotool/src/picotoolBuild-stamp/${subDir}")
 endforeach()
 if(cfgdir)
-  file(MAKE_DIRECTORY "/Users/a81802/Documents/Programs/test1/picotool/src/picotoolBuild-stamp${cfgdir}") # cfgdir has leading slash
+  file(MAKE_DIRECTORY "/Users/a81802/Documents/Programs/groupc_fm/picotool/src/picotoolBuild-stamp${cfgdir}") # cfgdir has leading slash
 endif()
